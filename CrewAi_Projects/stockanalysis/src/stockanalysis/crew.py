72889067df0e748ae1ac4384cafe6d9c6d2eaa5e
Romulo Lima <romulo.lima@easypallet.com.br>
2024-12-14 20:07:17 -0500

    implementa armazenamento dos resultados

--- CrewAi_Projects/stockanalysis/src/stockanalysis/crew.py
+++ CrewAi_Projects/stockanalysis/src/stockanalysis/crew.py
@@ -113,3 +113,174 @@ class StockAnalysisCrew:
 			verbose=True,
 			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 		)
+
+@CrewBase
+class GenerateResearchReportCrew:
+	"""Research Report Crew"""
+	llm = LLM(
+		model="llama-3.3-70b-versatile",
+		api_key=GROQ_API_KEY,
+		base_url="https://api.groq.com/openai/v1"
+	)
+
+	@agent
+	def research_analyst_agent(self) -> Agent:
+		return Agent(
+			config=self.agents_config['research_analyst_agent'],
+			tools=[
+				scrape_and_summarize_website,
+				search_internet,
+				calculate,
+				search_10q,
+				search_news_on_yahoo,
+			],
+			verbose=True,
+			max_iter=30,
+			llm=self.llm
+		)
+
+
+	@task
+	def research_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['research_task'],
+		)
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the Stockanalysis crew"""
+		return Crew(
+			agents=self.agents,
+			tasks= self.tasks,
+			process=Process.sequential,
+			verbose=True,
+		)	
+	
+
+@CrewBase
+class GenerateFinancialReportCrew:
+	"""Financial Report Crew"""
+	llm = LLM(
+		model="llama-3.3-70b-versatile",
+		api_key=GROQ_API_KEY,
+		base_url="https://api.groq.com/openai/v1"
+	)
+
+	@agent
+	def financial_analyst_agent(self) -> Agent:
+		return Agent(
+			config=self.agents_config['financial_analyst_agent'],
+			verbose=True,
+			tools=[
+			scrape_and_summarize_website,
+			search_internet,
+			search_news,	
+			search_10q,
+			search_10k,
+			search_news_on_yahoo,
+      		],
+			llm=self.llm,
+			max_iter=30
+		)
+
+
+	@task
+	def financial_analysis_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['financial_analysis_task'],
+		)
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the Stockanalysis crew"""
+		return Crew(
+			agents=self.agents,
+			tasks= self.tasks,
+			process=Process.sequential,
+			verbose=True,
+		)		
+	
+@CrewBase
+class GenerateFillingReportCrew:
+	"""Filling Report Crew"""
+	llm = LLM(
+		model="llama-3.3-70b-versatile",
+		api_key=GROQ_API_KEY,
+		base_url="https://api.groq.com/openai/v1"
+	)
+
+	@agent
+	def financial_analyst_agent(self) -> Agent:
+		return Agent(
+			config=self.agents_config['financial_analyst_agent'],
+			verbose=True,
+			tools=[
+			scrape_and_summarize_website,
+			search_internet,
+			search_news,	
+			search_10q,
+			search_10k,
+			search_news_on_yahoo,
+      		],
+			llm=self.llm,
+			max_iter=30
+		)
+
+
+	@task
+	def filings_analysis_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['filings_analysis_task'],
+		)
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the Stockanalysis crew"""
+		return Crew(
+			agents=self.agents,
+			tasks= self.tasks,
+			process=Process.sequential,
+			verbose=True,
+		)	
+
+@CrewBase
+class GenerateRecommendReportCrew:
+	"""Recommend Report Crew"""
+	llm = LLM(
+		model="llama-3.3-70b-versatile",
+		api_key=GROQ_API_KEY,
+		base_url="https://api.groq.com/openai/v1"
+	)
+
+	@agent
+	def investment_advisor_agent(self) -> Agent:
+		return Agent(
+			config=self.agents_config['investment_advisor_agent'],
+			verbose=True,
+			tools=[
+				scrape_and_summarize_website,
+				search_internet,
+				search_news,
+				calculate,
+				search_news_on_yahoo
+      		],
+			llm=self.llm,
+			max_iter=30
+		)
+
+
+	@task
+	def recommend_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['recommend_task'],
+		)
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the Stockanalysis crew"""
+		return Crew(
+			agents=self.agents,
+			tasks= self.tasks,
+			process=Process.sequential,
+			verbose=True,
+		)				
--- CrewAi_Projects/stockanalysis/src/stockanalysis/main.py
+++ CrewAi_Projects/stockanalysis/src/stockanalysis/main.py
@@ -1,5 +1,97 @@
 #!/usr/bin/env python
-from CrewAi_Projects.stockanalysis.src.stockanalysis.crew import StockAnalysisCrew   
+from CrewAi_Projects.stockanalysis.src.stockanalysis.crew import StockAnalysisCrew, GenerateResearchReportCrew, GenerateFinancialReportCrew, GenerateFillingReportCrew, GenerateRecommendReportCrew
+import os
+
+
+def execute_stock_analysis():
+  company = "PLTR"
+  
+  financial_crew = FinancialCrew(company)
+  result = financial_crew.run()
+  
+  unique_recommend_directory_path = "CrewAi_Projects/stockanalysis/src/stockanalysis/reports/unique_recommend"
+  report_name = f"unique_recommend_report_{len(os.listdir(unique_recommend_directory_path))}" 
+  full_path = f"{unique_recommend_directory_path}/{report_name}.txt"
+  with open(full_path, "w+") as f:
+    f.write(str(result))       
+
+def generate_research_report():
+  company = "PLTR"
+  
+  research_crew = ResearchReportCrew(company)
+  result = research_crew.run()
+  
+  print(result)
+  research_directory_path = "CrewAi_Projects/stockanalysis/src/stockanalysis/reports/research"
+  report_name = f"research_report_{len(os.listdir(research_directory_path))}" 
+  full_path = f"{research_directory_path}/{report_name}.txt"
+  with open(full_path, "w+") as f:
+    f.write(str(result))    
+
+def generate_financial_report():
+  company = "PLTR"
+  research_directory_path = "CrewAi_Projects/stockanalysis/src/stockanalysis/reports/research"
+  report_name = "research_report_0" 
+  full_path = f"{research_directory_path}/{report_name}.txt"
+  with open(full_path, 'r') as arquivo:
+    summary = arquivo.read()
+
+  previousSummary = f"""
+  Previous Summary Report: 
+  {summary}
+  """    
+  
+  research_crew = FinancialReportCrew(company, previousSummary)
+  result = research_crew.run()
+  
+  financial_directory_path = "CrewAi_Projects/stockanalysis/src/stockanalysis/reports/financial_analysis"
+  report_name = f"financial_analysis_report_{len(os.listdir(financial_directory_path))}" 
+  full_path = f"{financial_directory_path}/{report_name}.txt"
+  with open(full_path, "w+") as f:
+    f.write(str(result)) 
+
+def generate_filling_report():
+  company = "PLTR"
+  financial_directory_path = "CrewAi_Projects/stockanalysis/src/stockanalysis/reports/financial_analysis"
+  report_name = "financial_analysis_report_0" 
+  full_path = f"{financial_directory_path}/{report_name}.txt"
+  with open(full_path, 'r') as arquivo:
+    summary = arquivo.read()
+  
+  previousSummary = f"""
+  Previous Summary Report: 
+  {summary}
+  """
+  research_crew = FillingAnalysisReportCrew(company, previousSummary)
+  result = research_crew.run()
+  
+  filling_directory_path = "CrewAi_Projects/stockanalysis/src/stockanalysis/reports/filling_analysis"
+  report_name = f"filling_analysis_report_{len(os.listdir(filling_directory_path))}" 
+  full_path = f"{filling_directory_path}/{report_name}.txt"
+  with open(full_path, "w+") as f:
+    f.write(str(result))   
+
+def generate_recommend_report():
+  company = "PLTR"
+  filling_directory_path = "CrewAi_Projects/stockanalysis/src/stockanalysis/reports/filling_analysis"
+  report_name = "filling_analysis_report_0" 
+  full_path = f"{filling_directory_path}/{report_name}.txt"
+  with open(full_path, 'r') as arquivo:
+    summary = arquivo.read()
+  
+  previousSummary = f"""
+  Previous Summary Report: 
+  {summary}
+  """
+  research_crew = RecommendReportCrew(company, previousSummary)
+  result = research_crew.run()
+  
+  recommend_directory_path = "CrewAi_Projects/stockanalysis/src/stockanalysis/reports/recommend"
+  report_name = f"recommend_report_{len(os.listdir(recommend_directory_path))}" 
+  full_path = f"{recommend_directory_path}/{report_name}.txt"
+  with open(full_path, "w+") as f:
+    f.write(str(result))       
+
 
 class FinancialCrew:
   
@@ -9,22 +101,71 @@ class FinancialCrew:
   def run(self):
     inputs = {
         "TipSection": "If you do your BEST WORK, I'll give you a $10,000 commission!",
-        "Company": "PLTR"
+        "Company": f"{self.company}",
+        "PreviousSummary": ""
     }
     
     result = StockAnalysisCrew().crew().kickoff(inputs=inputs)
     return result
-
-def execute_stock_analysis():
-  print("## Welcome to Financial Analysis Crew")
-  print('-------------------------------')
-  company = "PLTR"
   
-  financial_crew = FinancialCrew(company)
-  result = financial_crew.run()
-  print("\n\n########################")
-  print("## Here is the Report")
-  print("########################\n")
-  print(result)
-  with open("report.txt", "w+") as f:
-    f.write(str(result))
+class ResearchReportCrew:
+  
+  def __init__(self, company):
+    self.company = company
+
+  def run(self):
+    inputs = {
+        "TipSection": "If you do your BEST WORK, I'll give you a $10,000 commission!",
+        "Company": f"{self.company}",
+    }
+    
+    result = GenerateResearchReportCrew().crew().kickoff(inputs=inputs)
+    return result      
+  
+class FinancialReportCrew:
+  
+  def __init__(self, company, previousSummary):
+    self.company = company
+    self.previousSummary = previousSummary
+
+  def run(self):
+    inputs = {
+        "TipSection": "If you do your BEST WORK, I'll give you a $10,000 commission!",
+        "Company": f"{self.company}",
+        "PreviousSummary": f"{self.previousSummary}"
+    }
+    
+    result = GenerateFinancialReportCrew().crew().kickoff(inputs=inputs)
+    return result   
+
+class FillingAnalysisReportCrew:
+  
+  def __init__(self, company, previousSummary):
+    self.company = company
+    self.previousSummary = previousSummary
+
+  def run(self):
+    inputs = {
+        "TipSection": "If you do your BEST WORK, I'll give you a $10,000 commission!",
+        "Company": f"{self.company}",
+        "PreviousSummary": f"{self.previousSummary}"
+    }
+    
+    result = GenerateFillingReportCrew().crew().kickoff(inputs=inputs)
+    return result    
+  
+class RecommendReportCrew:
+  
+  def __init__(self, company, previousSummary):
+    self.company = company
+    self.previousSummary = previousSummary
+
+  def run(self):
+    inputs = {
+        "TipSection": "If you do your BEST WORK, I'll give you a $10,000 commission!",
+        "Company": f"{self.company}",
+        "PreviousSummary": f"{self.previousSummary}"
+    }
+    
+    result = GenerateRecommendReportCrew().crew().kickoff(inputs=inputs)
+    return result   
